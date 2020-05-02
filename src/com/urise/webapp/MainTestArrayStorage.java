9e3c8690d756a2a1eb0e21b51078b91b2277ac08
Pavel Shekholay <pavlik198926@gmail.com>
2020-05-02 13:43:50 +0300

    Fixed bugs lesson_2 hw_2 #2

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -21,7 +21,7 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
 
-        System.out.println("\nGet r4: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("\nGet r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
