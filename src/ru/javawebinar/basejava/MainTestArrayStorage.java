7a978712f7668ab1c52f11adab78cfbe11fda54f
Pavel Shekholay <pavlik198926@gmail.com>
2020-05-12 22:13:40 +0300

    Lesson3 hw3 #2

---
+++ src/ru/javawebinar/basejava/MainTestArrayStorage.java
@@ -0,0 +1,49 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.SortedArrayStorage;
+import ru.javawebinar.basejava.storage.Storage;
+
+/**
+ * Test for your ru.javawebinar.basejava.storage.Storage implementation
+ */
+public class MainTestArrayStorage {
+    static final Storage ARRAY_STORAGE = new SortedArrayStorage()   ;
+
+    public static void main(String[] args) {
+        Resume r1 = new Resume();
+        r1.setUuid("uuid1");
+        Resume r2 = new Resume();
+        r2.setUuid("uuid2");
+        Resume r3 = new Resume();
+        r3.setUuid("uuid3");
+
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
+
+        System.out.println("\nGet r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+
+        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+
+        ARRAY_STORAGE.update(r1);
+
+        printAll();
+        ARRAY_STORAGE.delete(r1.getUuid());
+
+        printAll();
+        ARRAY_STORAGE.clear();
+
+        printAll();
+
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+    }
+
+    static void printAll() {
+        System.out.println("\nGet All");
+        for (Resume r : ARRAY_STORAGE.getAll()) {
+            System.out.println(r);
+        }
+    }
+}
